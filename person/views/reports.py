603c94d1322b8a9be69b0a898e075fce7ce6945e
cesargodoi <cesar.godoi@gmail.com>
2022-12-05 17:03:01 -0300

    Creating - installed per period report

--- person/views/reports.py
+++ person/views/reports.py
@@ -1,8 +1,15 @@
-from django.shortcuts import render
+import pandas as pd
 
+# from datetime import datetime
+
+from django.shortcuts import render
 from django.contrib.auth.decorators import login_required, permission_required
+from django.utils.translation import gettext as _
+from django.urls import reverse
+
 from ..models import Person
 from center.models import Responsible
+from base.utils import get_person_dict
 
 
 @login_required
@@ -17,3 +24,42 @@ def person_badge(request, person_id):
         "responsibles": responsibles,
     }
     return render(request, template, context)
+
+
+@login_required
+@permission_required("publicwork.view_lecture")
+def installed_per_period(request):
+    if request.GET.get("dt1") and request.GET.get("dt2"):
+        # get person dict
+        _dict = get_person_dict(request, Person)
+        if _dict:
+            # select columns to report
+            columns = [
+                "name",
+                "local",
+                "status",
+                "aspect",
+                "date",
+            ]
+            # generate pandas dataframe
+            report_data = pd.DataFrame(_dict, columns=columns)
+            #  adjust index
+            report_data.index += 1
+
+            context = {
+                "title": _("installed per period"),
+                "subtitle": request.user.person.center,
+                "report_data": report_data.to_html(),
+                "goback": reverse("person_home") + "?init=on",
+                "search": "base/searchs/modal_period.html",
+            }
+
+            return render(request, "base/reports/show_report.html", context)
+
+    context = {
+        "title": _("installed per period"),
+        "goback": reverse("person_home") + "?init=on",
+        "search": "base/searchs/modal_period.html",
+    }
+
+    return render(request, "base/reports/show_report.html", context)
